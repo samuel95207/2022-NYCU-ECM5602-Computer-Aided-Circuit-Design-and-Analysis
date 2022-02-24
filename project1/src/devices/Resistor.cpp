61c9e017484d7c3e23cae5ea4b15ccfcdc772579
Samuel Hunag <lspss95207@gmail.com>
2022-02-24 16:55:34 +0800

    Fix resistor type typo

--- project1/src/devices/Resistor.cpp
+++ project1/src/devices/Resistor.cpp
@@ -3,7 +3,7 @@
 Resistor::Resistor(string name_in, string node_p, string node_n, double value_in, string group_in) {
     name = name_in;
     index = atoi(name.substr(1).c_str());
-    type = DeviceType::CAPACITOR;
+    type = DeviceType::RESISTOR;
     nodes.resize(2);
     nodes[0] = node_p;
     nodes[1] = node_n;
