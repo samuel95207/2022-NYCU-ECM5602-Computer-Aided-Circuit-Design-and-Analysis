ab6dbb45beb3a527be430f0e383f11dd019869c9
Samuel Hunag <lspss95207@gmail.com>
2022-02-25 01:32:47 +0800

    Add [] back to print

--- project1/src/Matrix.cpp
+++ project1/src/Matrix.cpp
@@ -132,7 +132,21 @@ bool Matrix::isSquare() const { return row == col && isValid(); }
 bool Matrix::isValid() const { return row > 0 && col > 0; }
 
 
-void Matrix::print() const { cout << *this; }
+void Matrix::print() const {
+    cout << "[ ";
+    for (int i = 0; i < row; i++) {
+        if (i != 0) {
+            cout << "  ";
+        }
+        for (int j = 0; j < col; j++) {
+            cout << data[i][j] << " ";
+        }
+        if (i != row - 1) {
+            cout << "\n";
+        }
+    }
+    cout << "]\n";
+}
 
 
 Matrix Matrix::transpose() const {
