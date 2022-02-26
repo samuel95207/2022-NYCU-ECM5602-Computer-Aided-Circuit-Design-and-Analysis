d556d8f1061e6e23e0b95700a3062ad50103031a
Samuel Hunag <lspss95207@gmail.com>
2022-02-26 18:50:39 +0800

    Add capacitor stamps

--- project1/src/devices/Capacitor.cpp
+++ project1/src/devices/Capacitor.cpp
@@ -11,4 +11,34 @@ Capacitor::Capacitor(string name_in, string node_p, string node_n, double value_
 }
 
 
-pair<Matrix, Matrix> Capacitor::stampMatrix() const { return pair<Matrix, Matrix>(Matrix(), Matrix()); }
+pair<Matrix, Matrix> Capacitor::stampMatrix() const {
+    Matrix mnaStamp = Matrix();
+    Matrix rhsStamp = Matrix();
+    if (group == "G2" || group == "g2") {
+        if (nodes[0] == "0" && nodes[1] == "0") {
+            mnaStamp = Matrix({{1.0}});
+            rhsStamp = Matrix(1, 1);
+        } else if (nodes[0] == "0") {
+            mnaStamp = Matrix({{0.0, -1.0}, {value, 1.0}});
+            rhsStamp = Matrix(2, 1);
+        } else if (nodes[1] == "0") {
+            mnaStamp = Matrix({{0.0, 1.0}, {-value, 1.0}});
+            rhsStamp = Matrix(2, 1);
+        } else {
+            mnaStamp = Matrix({{0.0, 0.0, 1.0}, {0.0, 0.0, -1.0}, {-value, value, 1.0}});
+            rhsStamp = Matrix(3, 1);
+        }
+    } else {
+        if (nodes[0] == "0" && nodes[1] == "0") {
+            mnaStamp = Matrix();
+            rhsStamp = Matrix();
+        } else if (nodes[0] == "0" || nodes[1] == "0") {
+            mnaStamp = Matrix({{value}});
+            rhsStamp = Matrix(1, 1);
+        } else {
+            mnaStamp = Matrix({{value, -value}, {-value, value}});
+            rhsStamp = Matrix(2, 1);
+        }
+    }
+    return pair<Matrix, Matrix>(mnaStamp, rhsStamp);
+}
