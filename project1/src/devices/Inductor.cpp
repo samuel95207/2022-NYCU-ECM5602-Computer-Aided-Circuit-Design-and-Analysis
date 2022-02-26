d516768943fde81e2cd111780a659f79a9b67bfe
Samuel Hunag <lspss95207@gmail.com>
2022-02-26 18:50:51 +0800

    Add inductor stamps

--- project1/src/devices/Inductor.cpp
+++ project1/src/devices/Inductor.cpp
@@ -10,6 +10,22 @@ Inductor::Inductor(string name_in, string node_p, string node_n, double value_in
     value = value_in;
 }
 
- pair<Matrix, Matrix>Inductor::stampMatrix() const{
-    return pair<Matrix, Matrix>(Matrix(), Matrix());
+pair<Matrix, Matrix> Inductor::stampMatrix() const {
+    Matrix mnaStamp = Matrix();
+    Matrix rhsStamp = Matrix();
+    if (nodes[0] == "0" && nodes[1] == "0") {
+        mnaStamp = Matrix({{value}});
+        rhsStamp = Matrix(1, 1);
+    } else if (nodes[0] == "0") {
+        mnaStamp = Matrix({{0.0, -1.0}, {1.0, value}});
+        rhsStamp = Matrix(2, 1);
+    } else if (nodes[1] == "0") {
+        mnaStamp = Matrix({{0.0, 1.0}, {-1.0, value}});
+        rhsStamp = Matrix(2, 1);
+    } else {
+        mnaStamp = Matrix({{0.0, 0.0, 1.0}, {0.0, 0.0, -1.0}, {-1.0, 1.0, value}});
+        rhsStamp = Matrix(3, 1);
+    }
+
+    return pair<Matrix, Matrix>(mnaStamp, rhsStamp);
 }
